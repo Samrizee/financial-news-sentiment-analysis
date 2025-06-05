9d01dfdf78ca3c73cd3c141f62a76504c4414376
samrizee <samrawitzerfu@gmail.com>
2025-06-05 05:10:11 +0300

    renamed the py file

--- scripts/TA-Lib_Indicator.py
+++
@@ -1,7 +0,0 @@
-import talib
-
-def Technical_Analyser(data):
-    data['SMA_20'] = talib.SMA(data['Close'], timeperiod=20)
-    data['RSI'] = talib.RSI(data['Close'], timeperiod=14)
-    data['MACD'], data['MACD_signal'], _ = talib.MACD(data['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
-    print(data[['Close', 'SMA_20', 'RSI', 'MACD', 'MACD_signal']].head())
